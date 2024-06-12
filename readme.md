48c7f36587fc6c579f4fdf51dc458f1c1bcbd9a0
WillTrem <wt22@videotron.ca>
2024-06-12 16:34:09 -0400

    Fixed product data detection for schema.org websites + readme + manifest updates

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Instructions
+
+## Dev instructions
+1. In a terminal window, run `npm run dev` to run tailwind css.
+2. In a second terminal window, run `npm run start:firefox` to open the firefox sandbox environment.
+3. You're good to go! You can develop the extension with hot reloads (on the firefox sandbox environment).
+
+## Build & Package Instructions
+1. In a termainl window, run `npm run build`
--- src/contentScript.js
+++ src/contentScript.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("load", getProductInfo);
 
+// TODO: Execute getProductInfo whenever the url changes
+
 browser.runtime.onMessage.addListener((message) => {
   // console.log(JSON.stringify(product));
   // alert("Open the Browser Console.");
@@ -13,6 +15,7 @@ browser.runtime.onMessage.addListener((message) => {
   }
 });
 
+// For shopify type websites (following schema.org conventions)
 function getProductInfo() {
   const productXpath = "//*[contains(text(),'schema.org')]";
 
@@ -26,37 +29,67 @@ function getProductInfo() {
 
   // If a product schema has been found
   if (productElement) {
-    let product;
+    let productMatch;
     try {
-      product = JSON.parse(filterNDJSON(productElement.textContent));
+      productMatch = JSON.parse(filterNDJSON(productElement.textContent));
     } catch (e) {
       console.log("ERROR: JSON object could not be parsed.");
       console.log(e);
     }
-    console.log(product);
-    // If an actual product from the schema has been found
-    if (product && product.name && product.offers) {
-      const productName = product.name;
-      const productOffer = product.offers[0] || product.offers;
-      const productPrice = productOffer.price;
-      console.log(productName + ", " + productPrice);
+    console.log(productMatch);
 
-      //alert(productName + ", " + productPrice);
-      sendMessageToBackground("set-product", {
-        name: productName,
-        price: productPrice,
-        url: productOffer.url,
-        brand: product.brand.name,
-      });
+    // If no product has been found
+    if(!productMatch){
+      sendMessageToBackground("clear-product");
+    }
+    // If a product has been found and its data is structured in an '@graph' array (schema.org)
+    else if (productMatch['@graph'] && Array.isArray(productMatch['@graph'])){
+      const pageElements = productMatch['@graph'];
+      const productElement = pageElements.find((e) => e['@type']==='Product');
+      const webSiteElement = pageElements.find((e) => e['@type']==='WebSite');
+      if(productElement){
+        console.log(productElement);
+        const product = extractDataFromProductObject(productElement, webSiteElement?.name);
+        sendMessageToBackground("set-product", product)
+      }
+      else{
+        sendMessageToBackground("clear-product");
+      }
+    }
+    // If a product has been found and the product is directly an '@type'='Product' object (schema.org)
+    else if(productMatch['@type'] && productMatch['@type']==='Product'){
+      if (productMatch && productMatch.name && productMatch.offers) {
+        const product = extractDataFromProductObject(productMatch);
+        sendMessageToBackground("set-product", product);
+      } 
     } else {
       sendMessageToBackground("clear-product");
     }
-  } else {
-    sendMessageToBackground("clear-product");
+    }
+    else {
+      sendMessageToBackground("clear-product");
+    }
+}
+
+function extractDataFromProductObject(productObject, brand = ''){
+  const productName = productObject.name;
+  const productOffer = productObject.offers[0] || productObject.offers;
+  const productURL = window.location.href;
+  const productPrice = productOffer.priceSpecification?.price || productOffer.price;
+  const productBrand = productObject?.brand?.name || brand;
+
+  console.log(productName + ", " + productPrice);
+
+  return {
+    name: productName,
+    price: productPrice,
+    url: productURL,
+    brand: productBrand,
   }
 }
 
 function sendMessageToBackground(type, content = undefined) {
+  console.log(content);
   browser.runtime.sendMessage({
     type,
     content,
